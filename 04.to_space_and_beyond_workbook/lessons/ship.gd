c957887c509308927adc91fb4a9ce423b5f50dfa
raweggyolk <edtso@s.sfusd.edu>
2024-03-18 14:31:31 -0700

    L5 : Making the ship move smoothly with steering

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,9 +1,10 @@
 extends Sprite2D
 
 var velocity := Vector2(480, -480)
-var max_speed := 600.0
+var max_speed := normal_speed
 var boost_speed := 1500.0
 var normal_speed := 600.0
+var steering_factor := 10.0
 
 func _ready():
 	pass # Replace with function body.
@@ -14,13 +15,19 @@ func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
+	
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
-	velocity = direction * max_speed
+		
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
+	
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
 
