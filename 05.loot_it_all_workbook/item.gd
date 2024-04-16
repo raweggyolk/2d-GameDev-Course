153e5ddb72f8eda6e9ca6f9dd7d2c9f1fa89573f
raweggyolk <edtso@s.sfusd.edu>
2024-04-16 13:54:20 -0700

    L9: Keep The Ship On Screen

--- 05.loot_it_all_workbook/item.gd
+++ 05.loot_it_all_workbook/item.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	var tween := create_tween()
+	var sprite2d := get_node("Sprite2D")
+	var target_position := Vector2(500, 400)
+	var animation_duration := 2.0
+	tween.tween_property(sprite_2d, "position", target_position, animation_duration)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
