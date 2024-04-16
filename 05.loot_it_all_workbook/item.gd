429b7f89901ea90cdbb52e888ada19e94ca057ae
raweggyolk <edtso@s.sfusd.edu>
2024-04-16 14:24:09 -0700

    L10: Make The Items Float

--- 05.loot_it_all_workbook/item.gd
+++ 05.loot_it_all_workbook/item.gd
@@ -4,11 +4,20 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	play_floating_animation()
+	
+	
+	
+func play_floating_animation() -> void:
 	var tween := create_tween()
-	var sprite2d := get_node("Sprite2D")
-	var target_position := Vector2(500, 400)
-	var animation_duration := 2.0
-	tween.tween_property(sprite_2d, "position", target_position, animation_duration)
+	var sprite_2d := get_node("Sprite2D")
+	var position_offset := Vector2(0.0, 4.0)
+	var duration := randf_range(0.8, 1.2)
+	tween.set_trans(Tween.TRANS_SINE)
+	sprite_2d.position.y = -1.0 * position_offset.y
+	tween.tween_property(sprite_2d, "position", position_offset, duration)
+	tween.tween_property(sprite_2d, "position",  -1.0 * position_offset, duration)
+	tween.set_loops()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
