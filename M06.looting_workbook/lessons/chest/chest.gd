e357ce3a40f06655881a5772efd71337d48150e7
raweggyolk <edtso@s.sfusd.edu>
2024-04-30 11:19:53 -0700

    L5. Open The Chest

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var is_open := false
 
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
 
 
 func _ready() -> void:
@@ -30,3 +35,20 @@ func set_outline_thickness(new_thickness: float) -> void:
 
 func _process(delta):
 	pass
+	
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	
+	if event_is_mouse_click:
+		open()
+
+
+
+
+
+
+
